9eaff191b6a606d1da3534e5059a076c823bc9b3
Daniel <DanielCarrete_Dev@outlook.com>
2021-01-02 18:42:25 -0600

    first commit

---
+++ funcione.py
@@ -0,0 +1,22 @@
+#def imprimir_mensaje():
+#    print("mensaje espacial:")
+#    print("bjbhbhjbhj")
+
+#imprimir_mensaje()
+#imprimir_mensaje()
+#imprimir_mensaje()
+def conversacion(mensaje):
+    print("hola")
+    print("como estas")
+    print(mensaje)
+    print("adios")
+
+opcion = int(imput('elige la opcion (1,2,3)'))
+if opcion == 1:
+    conversacion('elegistes la opcion 1')
+elif opcion == 2:
+    conversacion('elegistes la funcion 2')
+else:
+    print("escoge una opcion valida")
+
+       
